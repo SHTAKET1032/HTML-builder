abd2e050344e4dbf51b24c11c3d35443558e5417
SHTAKET1032 <SHTAKET1032@yandex.ru>
2022-11-07 18:22:13 +0300

    feat: add solution for 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,24 @@
+const fs = require("fs"),
+      path = require("path");
 
+const pathToFolder = path.parse(__filename);
+// console.log(pathToFolder.dir);
+
+fs.mkdir(`${pathToFolder.dir}/files-copy`, { recursive: true }, err => {
+  if(err) throw err;
+});
+
+fs.readdir(`${pathToFolder.dir}/files`, {withFileTypes: true}, (err, files) => {
+  if(err) throw err;
+  files.forEach(file => {
+    fs.readFile(`${pathToFolder.dir}/files/${file.name}`, (err, data) => {
+      if(err) throw err;
+      // console.log(`${pathToFolder.dir}/files/${file.name}`);
+
+      fs.writeFile(`${pathToFolder.dir}/files-copy/${file.name}`, `${data}`, (err) => {
+        if(err) throw err;
+        // console.log(`${pathToFolder.dir}/files-copy/${file.name}`);
+      });
+    });
+  });
+});
